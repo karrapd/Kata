0b5c86e65b64eda2430bcaa76bf1b736d1db153a
karrapd <cristina.dteodorescu@gmail.com>
2016-08-04 15:13:54 +0300

    Update CumulativeTriangle.py

--- CumulativeTriangle.py
+++ CumulativeTriangle.py
@@ -1,12 +1,12 @@
 def numbers_gen(n):
     while True:
-        n+=1
+        n += 1
         yield n
 
 def cumulative_triangle(n):
-    sum=0
-    p=((n-1)*(n))/2
-    f=numbers_gen(p)
-    for i in range(0,n):
-        sum+=f.next()
-    return sum
+    final_sum = 0
+    start_number = ((n-1)*(n))/2
+    numbers_list = numbers_gen(start_number)
+    for i in range(n):
+        final_sum + =numbers_list.next()
+    return final_sum
