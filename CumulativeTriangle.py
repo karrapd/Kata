38daea4365478e1b1bd4e1e621ac1661eefd6d9f
karrapd <cristina.dteodorescu@gmail.com>
2016-08-11 16:35:51 +0300

    Modified the calculation method

--- CumulativeTriangle.py
+++ CumulativeTriangle.py
@@ -1,12 +1,2 @@
-def numbers_gen(n):
-    while True:
-        n += 1
-        yield n
-
 def cumulative_triangle(n):
-    final_sum = 0
-    start_number = ((n-1)*(n))/2
-    numbers_list = numbers_gen(start_number)
-    for i in range(n):
-        final_sum + =numbers_list.next()
-    return final_sum
+    return n*((n-1)*(n+1)+2)/2
