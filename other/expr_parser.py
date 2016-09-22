a4abd0c3e556859895235dfd2e27b2d27972571d
karrapd <cristina.dteodorescu@gmail.com>
2016-09-22 16:33:24 +0300

    fixed return in filter_parens to return the correct lists

--- other/expr_parser.py
+++ other/expr_parser.py
@@ -182,7 +182,7 @@ class ExpressionTree:
                 tokens_list.append(tokens[i])
                 priorities_list.append(priorities[i])
 
-        return (tuples_list, priorities_list)
+        return (tokens_list, priorities_list)
 
     def __parse(self, tokens, priorities):
         # assuming expressions are always valid, if there's just one elem, it
