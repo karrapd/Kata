b25c99b8abdf8071be56255e52a82e200c56d1dc
karrapd <cristina.dteodorescu@gmail.com>
2016-09-30 15:25:09 +0300

    refactor calculation of sum and fixed eol issue on line 12

--- BestTravel.py
+++ BestTravel.py
@@ -4,11 +4,9 @@ import itertools
 def choose_best_sum(t, k, ls):
     possible_options = []
     for subset in itertools.combinations(ls, k):
-        sum = 0
-        for element in subset:
-            sum += element
-        if t-sum >= 0:
-            possible_options.append(sum)
+        s = sum(subset)
+        if s <= t:
+            possible_options.append(s)
     if len(possible_options) > 0:
         return sorted(possible_options)[-1]
     return None
