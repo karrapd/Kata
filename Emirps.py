9a42cf552649078eddbb76fb16d2bb922f0fb7a9
karrapd <cristina.dteodorescu@gmail.com>
2016-10-04 13:02:41 +0300

    add file - Kata Emirps

---
+++ Emirps.py
@@ -0,0 +1,43 @@
+class Emirps(object):
+
+    def __init__(self, n):
+        self.n = n
+
+    def __is_prime(self, number):
+        if number % 2 == 0 and number > 2:
+            return False
+        for i in range(3, int(number**0.5) + 1, 2):
+            if number % i == 0:
+                return False
+        return True
+
+    def __generate_primes(self):
+        dict = {}
+        primes = 2
+        while primes < self.n:
+            if primes not in dict:
+                yield primes
+                dict[primes * primes] = [primes]
+            else:
+                for num in dict[primes]:
+                    dict.setdefault(num + primes, []).append(num)
+                del dict[primes]
+            primes += 1
+
+    def __list_primes(self):
+        return [x for x in list(self.__generate_primes())
+                if self.__is_prime(int(str(x)[::-1])) and int(str(x)[::-1]) != x]
+
+    def find_emirp(self):
+        final_list = []
+        primes_list = self.__list_primes()
+        for element in primes_list:
+            if element > self.n:
+                break
+            final_list.append(element)
+        if final_list:
+            return [len(final_list), sorted(final_list)[-1], sum(final_list)]
+        return [0, 0, 0]
+
+emirp = Emirps(500000)
+print emirp.find_emirp()
